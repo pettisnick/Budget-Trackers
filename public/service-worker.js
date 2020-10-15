e2ddcc86c478fa589f054147d908f0a6fc549721
Nick Pettis <pettisnick@gmail.com>
2020-10-15 17:54:47 -0400

    registering serviceWorker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/style.css",
+    "/manifest.webmanifest",
     "/dist/bundle.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
