659dba2af98fb39f413aa1c2e14fed6f23c0d823
Nick Pettis <pettisnick@gmail.com>
2020-10-17 17:21:11 -0400

    final

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,9 @@
-const FILES_TO_CACHE = [
+console.log("Service worker is working!");
+
+const STATIC_CACHE = 'budget-trackers--v1';
+const DATA_CACHE = 'budget-trackers-data-v1';
+
+var FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/assets/css/style.css",
@@ -9,23 +14,22 @@ const FILES_TO_CACHE = [
     "/assets/icons/icon-512x512.png",
   ];
   
-  const STATIC_CACHE = "static-cache-v1";
-  const RUNTIME_CACHE = "runtime-cache";
   
   //Install
-  self.addEventListener("install", event => {
+  self.addEventListener('install', event => {
     event.waitUntil(
-      caches
-        .open(STATIC_CACHE)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(() => self.skipWaiting())
+      caches.open(DATA_CACHE)
+        .then(cache => {
+          return cache.addAll(FILES_TO_CACHE);
+        })
     );
+    self.skipWaiting();
   });
   
   //Activate
   // The activate handler takes care of cleaning up old caches.
   self.addEventListener("activate", event => {
-    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    const currentCaches = [STATIC_CACHE, DATA_CACHE];
     event.waitUntil(
       caches
         .keys()
@@ -47,48 +51,17 @@ const FILES_TO_CACHE = [
   });
   
   //Fetch
-  self.addEventListener("fetch", event => {
+  self.addEventListener('fetch', event => {
     // non GET requests are not cached and requests to other origins are not cached
-    if (
-      event.request.method !== "GET" ||
-      !event.request.url.startsWith(self.location.origin)
-    ) {
-      event.respondWith(fetch(event.request));
-      return;
-    }
-  
-    // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/images")) {
-      // make network request and fallback to cache if network request fails (offline)
-      event.respondWith(
-        caches.open(RUNTIME_CACHE).then(cache => {
-          return fetch(event.request)
-            .then(response => {
-              cache.put(event.request, response.clone());
-              return response;
-            })
-            .catch(() => caches.match(event.request));
-        })
-      );
-      return;
-    }
-  
-    // use cache first for all other requests for performance
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-  
-        // request is not in cache. make network request and cache the response
-        return caches.open(RUNTIME_CACHE).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
+      caches.open(STATIC_CACHE).then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request);
+    });
+  })
     );
-  });
+});
+  
+  
+    
   
